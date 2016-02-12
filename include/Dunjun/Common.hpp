2211a6ba186288d7a49932d34ff34acdc61c741a
SuddenVacuity <g.cggns@gmail.com>
2016-02-11 21:46:36 -0500

    Practice - Rogue-Like
    Made from the tutorial here:
    https://www.youtube.com/watch?v=fRUYl6_5m3o

---
+++ include/Dunjun/Common.hpp
@@ -0,0 +1,11 @@
+#ifndef DUNJUN_COMMON_HPP // ifndef checks if this macro has been defined
+#define DUNJUN_COMMON_HPP // if not then define it
+
+namespace Dunjun
+{
+	using Float32 = float; // sets Float32 to function as float
+	using Float64 = double; // sets Float64 to function as double
+} // END namespace Dunjun
+
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+
+//#include "../include/Dunjun/Common.hpp" // "../" means back one directory form the folder the .exe is in
+#include <Dunjun/Common.hpp> // set iclude folders in RMB(Dunjun)>>properties>>C++>>General>>Include
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	std::cout << "Hello World!\n";
+
+	std::cin.ignore();
+
+	return 0;
+}
